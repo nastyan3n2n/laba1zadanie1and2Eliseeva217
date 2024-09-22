f29e705846fc7edeaec0ca70a6ce417277bf9b4e
nastyan3n2n <158225567+nastyan3n2n@users.noreply.github.com>
2024-09-22 22:30:43 +0300

    Add files via upload

---
+++ laba1zadanie1and2Eliseeva217/src/Main.java
@@ -0,0 +1,21 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //zadanie1
+        /*double rubles = 30002.7;
+        double rateUSD = 78.5;
+        double dollar = rubles/rateUSD;
+        System.out.println("у вас на счету" + rubles + "рублей. В долларах это" + dollar + "долларов.")
+         */
+        //zadanie2
+        /*
+        string weather = "дождь";
+        if (weather == "дождь")
+            System.out.println("беру с собой зонт");
+        else
+            System.out.println("иду на легке");
+        System.out.println("люблю гулять");
+        */
+    }
+}
